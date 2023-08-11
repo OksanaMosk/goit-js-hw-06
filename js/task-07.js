d9cadab2cb56519cee31e810fd60bb6d907c0efe
OksanaMosk <130758466+OksanaMosk@users.noreply.github.com>
2023-08-11 17:01:32 +0300

    all

--- js/task-07.js
+++ js/task-07.js
@@ -1,11 +1,6 @@
 const sizeContr = document.querySelector("#font-size-control");
 const myText = document.querySelector("#text");
-const myRange = document.querySelector("range");
-const rangeMin = document.querySelector("min");
-const rangeMax = document.querySelector("max");
-myRange.addEventListener("mousedown", (event) => {
-  if (rangeMin.getAttribute("min") + 1) {
-    myText.textContent.style = "+1";
-  } else {
-  }
-});
+sizeContr.addEventListener("input", handler);
+function handler(event) {
+    myText.style.fontSize = event.currentTarget.value + 'px'
+};
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,12 @@
+const logEl = document.querySelector('.login-form');
+logEl.addEventListener('submit', handler);
+function handler(event) {
+    if (condition) {
+       alert(message) 
+    } else {
+        alert(' ')
+    }
+    event.preventDefault;
+}
 
+console.dir(message);
